b51c0a16d288a64a3988832846e37dbee3842bcb
Daban Sadiq <dabansadiq@icloud.com>
2021-06-15 09:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ function map(src, callback) {
   return r;
 }
 
-// function reduce(src, callback, startingValue) {
-//   let total;
-//   if (startingValue) {
-//     total = startingValue;
-//     for (let i = 0; i < src.length; i++) {
-//       total = callback(src[i], total);
-//     }
-//     return total;
-//   } else {
-//     total = src[0];
-//     for (let i = 1; i < src.length; i++) {
-//       total = callback(src[i], total);
-//     }
-//     return total;
-//   }
-// }
+function reduce(src, callback, startingValue) {
+  let total;
+  if (startingValue) {
+    total = startingValue;
+    // for (let i = 0; i < src.length; i++) {
+    //   total = callback(src[i], total);
+    // }
+    return total;
+  } else {
+    total = src[0];
+    for (let i = 1; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  }
+}
